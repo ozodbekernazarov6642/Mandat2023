3593c674c17c09c2ff39619b237ae4a74d209438
Sar-dor <sardorernazarov4555@gmail.com>
2023-08-03 22:53:32 +0500

    deploy heroku

--- data/config.py
+++ data/config.py
@@ -15,5 +15,5 @@ import os
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
 ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
-IP = str(os.environ.get("ip"))  # Xosting ip manzili
+IP = str(os.environ.get("ip"))  # Xosting ip manzil
 
